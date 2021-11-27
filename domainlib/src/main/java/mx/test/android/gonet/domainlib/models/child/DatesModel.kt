60d7e5a92673706b43dc66ebc9a047c8e81e0f2c
Mauricio Castillo Parra <mauricio.castillo@gonet.us>
2021-11-26 23:17:59 -0600

    Se agrega parcelable a modelo Dates

--- domainlib/src/main/java/mx/test/android/gonet/domainlib/models/child/DatesModel.kt
+++ domainlib/src/main/java/mx/test/android/gonet/domainlib/models/child/DatesModel.kt
@@ -1,12 +1,13 @@
 package mx.test.android.gonet.domainlib.models.child
 
+import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class DatesModel(
     var maximum: String,
     var minimum: String,
-) {
+): Parcelable {
     constructor() : this(
         maximum = "",
         minimum = ""
